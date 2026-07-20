241c73e98a2b420396373695359acdc398d93dc2
rayhanmawuntu-stack <rayhanmawuntu@gmail.com>
2026-07-20 11:04:46 +0700

    Match tracking header card styling

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,6 +1,6 @@
 (async function(){
   try{
-    const version='20260720-tracking-head-79';
+    const version='20260720-tracking-card-80';
     let initialTheme='light';
     let initialPalette='ksb';
     try{
